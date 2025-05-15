43a93f471b7c9618591b3eb48a529da813c0e74c
Gaël Pardonnet <gael.pardonnet@hotmail.fr>
2025-05-15 14:29:16 +0200

    feat création dé aléatoire js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-const div = document.createElement("div");
-const player = document.getElementById("player")
+window.addEventListener("load", createDice)
 
-div.classList.add("dice")
-player.appendChild(div)
+let diceDiv
+
+
+function diceRandom() {
+    return Math.round(Math.random() * (6 - 1) + 1);
+};
+
+function createDice() {
+    const div = document.createElement("div");
+    const player = document.getElementById("player")
+    div.classList.add("dice")
+    player.appendChild(div)
+    let randomNumber = diceRandom()
+    let position = (randomNumber-1)*100
+    div.style.backgroundPosition = `-${position}px 0`
+}
