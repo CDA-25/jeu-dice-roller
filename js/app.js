a458e7820061997b8dbf5cdb223e4052f6eaf221
Alice <alice.cordier39210@gmail.com>
2025-05-15 16:17:06 +0200

    etape 3 : choisir le nombre de dés

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 window.addEventListener("load", function () {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("dice");
-    console.log(newDiv);
-    const player = document.querySelector("#player");
-    player.appendChild(newDiv);
+
+
 
     function nbrAleatoire(min, max) {
         return Math.round(Math.random() * (max - min + 1) + min);
     }
 
-    const tirage = nbrAleatoire(1, 6);
-    console.log(tirage);
+    function creaDe() {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("dice");
+        console.log(newDiv);
+        const player = document.querySelector("#player");
+        player.appendChild(newDiv)
+        const tirage = nbrAleatoire(1, 6);
+        console.log(tirage);
+        const dicePosition = (tirage - 1) * -100;
+        newDiv.style.backgroundPosition = `${dicePosition}px`;
+    }
 
-    const dicePosition = (tirage - 1) * -100;
-    newDiv.style.backgroundPosition = `${dicePosition}px`;
 
+
+    function choisirNbr() {
+        let nbrChoisi = parseInt(prompt('Avec combien de dé veux-tu jouer ?'));
+        if (!isNaN(nbrChoisi) && nbrChoisi >= 1 && nbrChoisi <= 6) {
+            for (let i = 0; i < nbrChoisi; i++) {
+                creaDe()
+            }
+        } else {
+            alert('Vous devez entrer un chiffre entre 1 et 6');
+            choisirNbr()
+        }
+
+    }
+
+    choisirNbr()
 });
