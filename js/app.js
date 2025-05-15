11c98fe8e535236663bf61cee6fb41095a47419e
Alice <alice.cordier39210@gmail.com>
2025-05-15 14:58:14 +0200

    etape 2 : tirer un nombre aléatoire qui change la face du dé

--- js/app.js
+++ js/app.js
@@ -4,4 +4,15 @@ window.addEventListener("load", function () {
     console.log(newDiv);
     const player = document.querySelector("#player");
     player.appendChild(newDiv);
+
+    function nbrAleatoire(min, max) {
+        return Math.round(Math.random() * (max - min + 1) + min);
+    }
+
+    const tirage = nbrAleatoire(1, 6);
+    console.log(tirage);
+
+    const dicePosition = (tirage - 1) * -100;
+    newDiv.style.backgroundPosition = `${dicePosition}px`;
+
 });
