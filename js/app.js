2f800cbf4ea39fcc83fe1f04bf8d72eac93d06f1
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-05-15 16:27:02 +0200

    diceRoll sans design

--- js/app.js
+++ js/app.js
@@ -1,26 +1,45 @@
+let gameCount = 0
 const dicesInput = document.getElementById("chooseNumberDices")
 const dicesValue = document.getElementById("numberDices")
 dicesInput.addEventListener("click", function() {
     let nbDices = 0
     nbDices = dicesValue.value
+    
+    
 
-    for (let i = 0; i < nbDices; i++) {
-    const divPlayer = document.getElementById("player")
-    const playerDice = document.createElement("div")
-    playerDice.className = "dice"
-    playerDice.style.backgroundPositionX = randomNumber() + "px"
-    divPlayer.appendChild(playerDice)
+    if (gameCount === 0) {
+        startGame(nbDices)   
+    }
 
-    const divDealer = document.getElementById("dealer")
-    const dealerDice = document.createElement("div")
-    dealerDice.className = "board"
-    dealerDice.style.backgroundPositionX = randomNumber() + "px"
-    divDealer.appendChild(dealerDice)
-}
+    else {
+        document.getElementById("player").innerHTML = ""
+        document.getElementById("dealer").innerHTML = ""
+        startGame(nbDices)
+    }
 })
 
 function randomNumber() {
     return Math.round((Math.random() * 6) +1) * 100
 }
+
+
+function startGame(dices) {
+    for (let i = 0; i < dices; i++) {
+        let divPlayer = document.getElementById("player")
+        const playerDice = document.createElement("div")
+        playerDice.className = "dice"
+        playerDice.style.backgroundPositionX = randomNumber() + "px"
+        divPlayer.appendChild(playerDice)
+
+        let divDealer = document.getElementById("dealer")
+        const dealerDice = document.createElement("div")
+        dealerDice.className = "board"
+        dealerDice.style.backgroundPositionX = randomNumber() + "px"
+        divDealer.appendChild(dealerDice)
+    }
+    gameCount++
+}
+
+//Ca manquait de commentaires, alors j'en ai mis un ! :D
 
 
